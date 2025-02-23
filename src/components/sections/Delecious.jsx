69faac4be70f0122efbe7e248021518c5d381498
fenix1409 <b2550486@gmail.com>
2025-02-23 19:54:19 +0500

    refactor Delecious section images for responsive display

--- src/components/sections/Delecious.jsx
+++ src/components/sections/Delecious.jsx
@@ -11,8 +11,9 @@ const Delecious = () => {
                 <h1 className='text-[80px] max-sm:text-[48px] leading-[87px] max-sm:leading-[52px] font-extrabold mb-[63px] max-sm:mb-[35px] text-white'>We make <span className='text-[#237249]'>delicious</span></h1>
                 <p className='text-[20px] max-sm:text-[14px] leading-[32px] max-sm:leading-[22px] text-[#D9D9D9] mb-[63px] max-sm:mb-[38px]'>Only in 2021 we have made more than 100,000 orders for you, your loved ones, all of you, and in 2022 we are ready to destroy the market</p>
                 <div className="relative">
-                    <img src={PRoces} alt="image" width={255} height={163} />
-                    <Button title={"Cooking Process"} extraClass={'!w-[326px] max-sm:!w-[193px] !px-[42px] max-sm:!px-[25px] !py-[20px] max-sm:!py-[11px] !absolute !top-1/2 !left-1/2 max-sm:!left- !transform !-translate-x-1/2 !-translate-y-1/2'} />
+                    <img src={PRoces} alt="image" width={255} height={163} className='min-sm:hidden'/>
+                    <img src={PRoces} alt="image" width={447} height={275} className='max-sm:hidden'/>
+                    <Button title={"Cooking Process"} extraClass={'!w-[326px] max-sm:!w-[193px] !px-[42px] max-sm:!px-[25px] !py-[20px] max-sm:!py-[11px] !absolute !top-1/2 !left-1/2 max-sm:!left-[calc(1/2*77%)] !transform !-translate-x-1/2 !-translate-y-1/2'} />
                 </div>
             </div>
         </div>
